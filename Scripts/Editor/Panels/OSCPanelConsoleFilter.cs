da6afb23f0877cb9de809342957c9e2892037aab
Vladimir Sigalkin <_ext_@bk.ru>
2018-09-15 15:22:49 +0300

    Added Dropdown base

---
+++ Scripts/Editor/Panels/OSCPanelConsoleFilter.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright (c) 2018 ExT (V.Sigalkin) */
+
+using UnityEngine;
+using UnityEditor;
+
+using extOSC.Core;
+using extOSC.Core.Console;
+using extOSC.Editor.Windows;
+
+namespace extOSC.Editor.Panels
+{
+    public class OSCPanelConsoleFilter : OSCPanel
+    {
+        #region Public Methods
+
+        public OSCPanelConsoleFilter(OSCWindow parentWindow, string panelId) : base(parentWindow, panelId)
+        {
+        }
+
+        #endregion
+    }
+}
