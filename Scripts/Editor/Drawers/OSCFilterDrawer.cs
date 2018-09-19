4b95e97f9d18a2185d49dee447a4ba819a204d25
Vladimir Sigalkin <_ext_@bk.ru>
2018-09-19 14:10:01 +0300

    Added filter value saving

--- Scripts/Editor/Drawers/OSCFilterDrawer.cs
+++ Scripts/Editor/Drawers/OSCFilterDrawer.cs
@@ -35,28 +35,63 @@ namespace extOSC.Editor.Drawers
 
         public void Draw()
         {
-            GUI.SetNextControlName(_controlName);
-
-            var fieldPosition = GUILayoutUtility.GetRect(0, 350, 0, 100);
+            var fieldPosition = GUILayoutUtility.GetRect(0, 200, 0, 100);
             fieldPosition.y = 2;
+
+            var controlId = GUIUtility.GetControlID("TextField".GetHashCode(), FocusType.Keyboard) + 1;
+
+            GUI.SetNextControlName(_controlName);
 
             _filterValue = GUI.TextField(fieldPosition, _filterValue, OSCEditorStyles.SearchField);
 
-            if (Event.current.type != EventType.Layout)
+            ProcessKeys(controlId);
+
+            var controlName = GUI.GetNameOfFocusedControl();
+            if (controlName != _controlName && string.IsNullOrEmpty(_filterValue))
             {
-                var controlName = GUI.GetNameOfFocusedControl();
-                if (controlName != _controlName && string.IsNullOrEmpty(_filterValue))
-                {
-                    GUI.Label(fieldPosition, "Packet Filter", OSCEditorStyles.SearchFieldPlaceholder);
-                }
-                else
-                {
-                    GUIUtility.keyboardControl = 0;
-                }
+                GUI.Label(fieldPosition, "Packet Filter", OSCEditorStyles.SearchFieldPlaceholder);
             }
 
-            
         }
+
+        #endregion
+
+        #region Private Methods
+
+        // Small hack.
+        public void ProcessKeys(int controlId)
+        {
+            if (controlId == GUIUtility.keyboardControl)
+            {
+                if (Event.current.type == EventType.KeyUp && (Event.current.modifiers == EventModifiers.Control || Event.current.modifiers == EventModifiers.Command))
+                {
+                    if (Event.current.keyCode == KeyCode.C)
+                    {
+                        var editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
+                        editor.Copy();
+
+                        Event.current.Use();
+                    }
+                    else if (Event.current.keyCode == KeyCode.V)
+                    {
+                        var textEditor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
+                        textEditor.Paste();
+
+                        _filterValue = textEditor.text;
+
+                        Event.current.Use();
+                    }
+                    else if (Event.current.keyCode == KeyCode.A)
+                    {
+                        var textEditor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
+                        textEditor.SelectAll();
+
+                        Event.current.Use();
+                    }
+                }
+            }
+        }
+
 
         #endregion
     }
