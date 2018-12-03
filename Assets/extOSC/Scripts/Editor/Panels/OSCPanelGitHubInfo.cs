df61360c6e128475bc5a72dd97700a56280bc003
Vladimir Sigalkin <_ext_@bk.ru>
2018-12-03 17:03:00 +0300

    Start working on GitHub info page.

---
+++ Assets/extOSC/Scripts/Editor/Panels/OSCPanelGitHubInfo.cs
@@ -0,0 +1,28 @@
+﻿/* Copyright (c) 2018 ExT (V.Sigalkin) */
+
+using extOSC.Editor.Windows;
+
+namespace extOSC.Editor.Panels
+{
+    public class OSCPanelGitHubInfo : OSCPanel
+    {
+        #region Public Vars 
+
+        #endregion
+
+        #region Private Vars
+
+        #endregion
+
+        #region Public Methods
+
+        public OSCPanelGitHubInfo(OSCWindow parentWindow, string panelId) : base(parentWindow, panelId)
+        { }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
---
+++ Assets/extOSC/Scripts/Editor/Windows/OSCWindowGitHubInfo.cs
@@ -0,0 +1,30 @@
+﻿/* Copyright (c) 2018 ExT (V.Sigalkin) */
+
+using UnityEditor;
+using UnityEngine;
+
+using extOSC.Editor.Panels;
+
+
+namespace extOSC.Editor.Windows
+{
+    public class OSCWindowGitHubInfo : OSCWindow<OSCWindowGitHubInfo, OSCPanelGitHubInfo>
+    {
+        #region Public Vars
+
+        #endregion
+
+        #region Public Methods
+
+        [MenuItem("Window/extOSC/GitHub Info", false, 1040)]
+        public static void ShowWindow()
+        {
+            Instance.titleContent = new GUIContent("OSC Console", OSCEditorTextures.IronWallSmall);
+            Instance.minSize = new Vector2(610, 200);
+            Instance.maxSize = Instance.minSize;
+            Instance.ShowPopup();
+        }
+
+        #endregion
+    }
+}
