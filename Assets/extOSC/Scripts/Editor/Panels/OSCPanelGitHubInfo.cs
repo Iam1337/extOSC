fb2cf35024e3b9f14688ee62f3f0c9edfb743d7a
Vladimir Sigalkin <_ext_@bk.ru>
2018-12-14 15:23:59 +0300

    Some draft of GitHub Info

--- Assets/extOSC/Scripts/Editor/Panels/OSCPanelGitHubInfo.cs
+++ Assets/extOSC/Scripts/Editor/Panels/OSCPanelGitHubInfo.cs
@@ -1,6 +1,8 @@
 ﻿/* Copyright (c) 2018 ExT (V.Sigalkin) */
 
 using extOSC.Editor.Windows;
+using UnityEditor;
+using UnityEngine;
 
 namespace extOSC.Editor.Panels
 {
@@ -21,7 +23,36 @@ namespace extOSC.Editor.Panels
 
         #endregion
 
-        #region Private Methods
+        #region Protected Methods
+
+        protected override void DrawContent(Rect contentRect)
+        {
+            contentRect.position += new Vector2(5,5);
+            contentRect.size -= new Vector2(10, 10);
+
+            GUILayout.BeginArea(contentRect);
+
+            GUILayout.BeginVertical("box");
+
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Приветствие", OSCEditorStyles.CenterBoldLabel);
+            GUILayout.EndVertical();
+
+            GUILayout.Label("Текст.");
+
+
+            
+            GUILayout.BeginHorizontal("box");
+
+            var neverShow = EditorGUILayout.Toggle("Никогда не показывать", false);
+            var close = GUILayout.Button("Close");
+            
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+
+            GUILayout.EndArea();
+        }
 
         #endregion
     }
