3c18f9f3d62c58287e02c9822d91a66064c1bd66
Vladimir Sigalkin <_ext_@bk.ru>
2019-01-04 19:33:39 +0300

    Updated files header.

--- Assets/extOSC/Scripts/Core/Network/OSCReceiverWindowsStoreBackend.cs
+++ Assets/extOSC/Scripts/Core/Network/OSCReceiverWindowsStoreBackend.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2018 ExT (V.Sigalkin) */
+﻿/* Copyright (c) 2019 ExT (V.Sigalkin) */
 
 #if NETFX_CORE
 using UnityEngine;
--- Assets/extOSC/Scripts/Core/Packers/OSCPacker.cs
+++ Assets/extOSC/Scripts/Core/Packers/OSCPacker.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2018 ExT (V.Sigalkin) */
+﻿/* Copyright (c) 2019 ExT (V.Sigalkin) */
 
 using System.Linq;
 
