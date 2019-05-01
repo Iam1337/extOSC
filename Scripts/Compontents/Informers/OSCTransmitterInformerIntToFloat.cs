147c6376941cf591d3f38c6ecb4ef2af1df1850b
Vladimir Sigalkin <vladimir@sigalkin.ru>
2019-05-01 20:17:31 +0300

    Added two new components.

---
+++ Scripts/Compontents/Informers/OSCTransmitterInformerIntToFloat.cs
@@ -0,0 +1,19 @@
+﻿/* Copyright (c) 2019 ExT (V.Sigalkin) */
+
+using UnityEngine;
+
+namespace extOSC.Components.Informers
+{
+    [AddComponentMenu("extOSC/Components/Transmitter/Int To Float Informer")]
+    public class OSCTransmitterInformerIntToFloat : OSCTransmitterInformer<int>
+    {
+        #region Protected Methods
+
+        protected override void FillMessage(OSCMessage message, int value)
+        {
+            message.AddValue(OSCValue.Float(value));
+        }
+
+        #endregion
+    }
+}
