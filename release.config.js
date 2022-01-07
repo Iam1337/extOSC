3e55c240c69f5daed8538377abe2be166722013e
Vladimir Sigalkin <_ext_@bk.ru>
2022-01-07 22:01:07 +0300

    fix(ci): update semantic-release settings

---
+++ release.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+	"branches" : "main",
+	"tagFormat": "v${version}",
+	"plugins": [
+		["@semantic-release/commit-analyzer", {
+			"preset": "angular",
+			"releaseRules" : [
+				{"type": "docs", "release": "patch"},
+				{"type": "docs", "scope": "README", "release": false}
+			]
+		}],
+		["@semantic-release/release-notes-generator", {
+			"writerOpts": {
+				"commitsSort": ["scope", "subject"]
+			}
+		}],
+		["@semantic-release/changelog", {
+			"changelogFile": "CHANGELOG.md"
+		}],
+		["@semantic-release/npm", {
+			"npmPublish": false, 
+			"pkgRoot": `Assets/${process.env.PROJECT_NAME}` }],
+		["@semantic-release/git", {
+			"assets": [`Assets/${process.env.PROJECT_NAME}/package.json`, "CHANGELOG.md"],
+			"message": "chore(release): new release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+		}],
+		["@iam1337/create-unitypackage", {
+			"packageRoot": `Assets/${process.env.PROJECT_NAME}`,
+			"projectRoot": "./",
+			"output": `${process.env.PROJECT_NAME}.unitypackage`
+		}],
+		["@semantic-release/github", {
+			"assets": [
+				{"path": `${process.env.PROJECT_NAME}.unitypackage`, "label": `${process.env.PROJECT_NAME} v\${nextRelease.version}`}
+			]
+		}]
+	],
+	"preset": "angular"
+}
